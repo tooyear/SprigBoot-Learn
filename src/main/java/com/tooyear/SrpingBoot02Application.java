f0af8589322f85a7d6cfdb2362975e51c541a27e
tooyear <2316421531@qq.com>
2018-07-22 18:01:36 +0800

    第二章初始化

---
+++ src/main/java/com/tooyear/SrpingBoot02Application.java
@@ -0,0 +1,12 @@
+package com.tooyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingBoot02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingBoot02Application.class, args);
+    }
+}
